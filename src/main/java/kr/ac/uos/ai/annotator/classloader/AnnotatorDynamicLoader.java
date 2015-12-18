01fa330f0e4960fe99b6fe04ae0c920963e2aea2
lovebube <lovebube88@gmail.com>
2015-12-19 01:50:31 +0900

    Dynamic run jar pass!

--- src/main/java/kr/ac/uos/ai/annotator/classloader/AnnotatorDynamicLoader.java
+++ src/main/java/kr/ac/uos/ai/annotator/classloader/AnnotatorDynamicLoader.java
@@ -2,6 +2,14 @@ package kr.ac.uos.ai.annotator.classloader;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * @author Chan Yeon, Cho
  * @version 0.0.1 - SnapShot
@@ -10,6 +18,8 @@ import org.junit.Test;
  */
 
 public class AnnotatorDynamicLoader {
+
+    private String jarPath;
 
     public AnnotatorDynamicLoader() {
     }
@@ -20,6 +30,44 @@ public class AnnotatorDynamicLoader {
             Class cl = Class.forName("kr.ac.uos.ai.annotator.monitor.ResourceMonitor");
             Object obj = cl.newInstance();
         } catch (ClassNotFoundException e) {
+            System.out.println("Class not found!");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void loadClass2() {
+        this.jarPath = "Test.jar";
+        String path = System.getProperty("user.dir") + "\\lib\\" + jarPath;
+        File file = new File(path);
+        try {
+            URL url = file.toURL();
+            URL[] urls = new URL[]{url};
+            ClassLoader loader = new URLClassLoader(urls);
+//            Class<?> tempClass = loader.loadClass("kr.ac.uos.ai.Test");
+//            Constructor<?> constructor = tempClass.getConstructor();
+//            Object obj = constructor.newInstance();
+//            Method method = tempClass.getMethod("Test1");
+//            method.invoke(obj);
+
+            Class<?> tempClass = Class.forName("kr.ac.uos.ai.Test", true, loader);
+            Class<? extends Runnable> runClass = tempClass.asSubclass(Runnable.class);
+            Constructor<? extends Runnable> constructor = runClass.getConstructor();
+            Runnable doRun = constructor.newInstance();
+            Method method = tempClass.getMethod("Test1");
+            doRun.run();
+            method.invoke(doRun);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
             e.printStackTrace();
@@ -28,4 +76,11 @@ public class AnnotatorDynamicLoader {
         }
     }
 
+    public String getJarPath() {
+        return jarPath;
+    }
+
+    public void setJarPath(String jarPath) {
+        this.jarPath = jarPath;
+    }
 }
